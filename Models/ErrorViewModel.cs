fd70e897817ab658557ed2df4747503724bdf3d5
Arya <144851489+Aryahaseli@users.noreply.github.com>
2024-02-25 13:25:33 +0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Asp.net_odevi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
